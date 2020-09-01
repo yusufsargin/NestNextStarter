90f56f3967cd57b3687e8ad7aa8e0af48f8ffb67
yusufsargin <yusufsargin9@gmail.com>
2020-09-01 16:21:25 +0300

    Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { RenderModule } from 'nest-next';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import next from 'next/dist/server/next';
 
 @Module({
-  imports: [RenderModule],
+  imports: [
+    RenderModule.forRootAsync(
+      next({ dev: process.env.NODE_ENV !== 'production' }),
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
